14cfc8c00ef866b31cf8b534735365bcc1f22b6d
Hansel Kang <43307207+hanselkang@users.noreply.github.com>
2022-03-29 15:54:25 +0100

    exer a -1 complete

--- exercise_a_stops.py
+++ exercise_a_stops.py
@@ -2,6 +2,7 @@ stops = ["Croy", "Cumbernauld", "Falkirk High",
          "Linlithgow", "Livingston", "Haymarket"]
 
 # 1. Add "Edinburgh Waverley" to the end of the list
+stops.append("Edinburgh Waverley")
 # 2. Add "Glasgow Queen St" to the start of the list
 # 3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
 # 4. Print out the index position of "Linlithgow"
@@ -11,3 +12,5 @@ stops = ["Croy", "Cumbernauld", "Falkirk High",
 # 8. Sort the list alphabetically
 # 9. Reverse the positions of the stops in the list
 # 10 Print out all the stops using a for loop
+
+print(stops)
